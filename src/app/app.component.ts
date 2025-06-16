d33c1cc4f7f62810f5607f3cc42587d11e214029
Dmytro Ivasenko <dmtivasenko@gmail.com>
2025-06-16 08:59:57 +0400

    feat(taiga): setup taiga components and update angular.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component, Inject, input } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { TuiRoot } from '@taiga-ui/core';
 import { HeaderComponent } from './header/header.component';
-import { TuiIcon, TuiRoot } from '@taiga-ui/core';
 import { AsideComponent } from './aside/aside.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [HeaderComponent, AsideComponent],
+  imports: [HeaderComponent, AsideComponent, TuiRoot],
 })
 export class AppComponent {
   isSideBarOpen = input(true);
